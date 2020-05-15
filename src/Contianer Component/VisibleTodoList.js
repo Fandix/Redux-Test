c4b2bef6e190941cd55a570c81c5c9e98ca8fe14
Fandix Huang <fandix0925@gamil.com>
2020-05-15 20:59:43 +0800

    finish

--- src/Contianer Component/VisibleTodoList.js
+++ src/Contianer Component/VisibleTodoList.js
@@ -3,24 +3,26 @@ import ProductTable from "../Presentational Component/ProductTable";
 import { Products } from "../Products"
 
 const getInputVisibleData = (data,filter) => {
-    console.log(filter)
     const rows = [];
     let count = 0;
     data.forEach((product) => {
-        if(product.name.indexOf(filter.text) === -1)
-            return; 
-        if(product.stocked !== filter.isStock)
-            return;
-
+        if(filter.text){
+            if(product.name.indexOf(filter.text) === -1){
+                return;
+            }
+        }
+        if(filter.isStock)
+        {
+            if(product.stocked !== filter.isStock){
+                return;
+            }
+        }
         rows[count++] = product;
-        //rows.push(product);
     })
     return rows;
 };
 
 const mapStateToProps = (state) => {
-    getInputVisibleData(Products,state.InputdataFilter);
-    console.log(getInputVisibleData(Products,state.InputdataFilter));
     return {
         Products:getInputVisibleData(Products,state.InputdataFilter)
     }
--- src/Presentational Component/ProductTable.js
+++ src/Presentational Component/ProductTable.js
@@ -1,10 +1,49 @@
 import React from "react";
+import ProductRow from "./ProductRow";
+import ProductCategoryRow from "./ProductCategoryRow"
 
-const ProductTable = (props) => {
-    console.log(props)
-    return(
-        <div></div>
-    )
-};
+class ProductTable extends React.Component
+{
+    Products = () => {
+        const products = this.props.Products
+        const FilterProduct = [];
+        let lastCategory = "";
+
+        products.forEach(product => {
+            if(product.category !== lastCategory)
+            {
+                FilterProduct.push(
+                    <ProductCategoryRow 
+                        category={product.category}
+                        key={product.category}
+                    />
+                );
+            }
+            FilterProduct.push(
+                <ProductRow
+                    product={product}
+                    key={product.name}
+                />
+            )
+            lastCategory = product.category;
+        })
+        return FilterProduct;
+    };
+
+    render()
+    {
+        return(
+             <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>{this.Products()}</tbody>
+            </table>
+        )
+    }
+}
 
 export default ProductTable;
